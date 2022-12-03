e2e460a7c8cb2a1987ff427caee77b38e794991d
Nanar Boursalian <nboursalian@lyft.com>
2022-12-02 21:12:36 -0800

    Created home page' :

--- hotel-management/src/Pages/Home/Home.js
+++ hotel-management/src/Pages/Home/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 // import { Container, Row, Col, Button } from "reactstrap";
+import { Button } from "reactstrap";
+import { Link } from "react-router-dom";
 import "./home.css";
 
 export default class Home extends React.Component {
@@ -19,8 +21,18 @@ export default class Home extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Here is the home page</h1>
+            <div className="home-header">
+                <h1>Welcome to The Best Hotel!</h1>
+                <h2 className="console-title">Admin Console</h2>
+                <Button className="task-button" outline color="warning">
+                    <Link className="button-link" to="/add-reservation">Add Reservation</Link>
+                </Button>{' '}
+                <Button className="task-button" outline color="warning">
+                    <Link className="button-link" to="/reservations">See All Reservations</Link>
+                </Button>{' '}
+                <Button className="task-button" outline color="warning">
+                    <Link className="button-link" to="/employees">Manage Employee Information</Link>
+                </Button>{' '}
             </div>
         );
     }
