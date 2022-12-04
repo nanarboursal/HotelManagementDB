de61eada32911eb7f5d47c645508294349391a07
Nanar Boursalian <nboursalian@lyft.com>
2022-12-03 22:35:37 -0800

    Added credit card fk to reservation table

--- backend/schema.sql
+++ backend/schema.sql
@@ -37,6 +37,7 @@ CREATE TABLE RESERVATION(
     Reservation_ID char(5) not null,
     No_Rooms int not null,
     Guest_ID char(5) not null,
+    Credit_Card_ID char(5) not null,
     No_Guests int not null,
     Preferences varchar(50),
     Booking_Date date not null,
@@ -45,6 +46,7 @@ CREATE TABLE RESERVATION(
     Reservation_End_Date date not null,
     No_Nights int not null,
     CONSTRAINT res_guest_fk FOREIGN KEY(Guest_ID) REFERENCES GUEST(Guest_ID),
+    CONSTRAINT res_cc_fk FOREIGN KEY(Credit_Card_ID) REFERENCES CREDIT_CARD(Credit_Card_ID),
 );
 
 DROP TABLE IF EXISTS BOOKED_ROOM;
