374dc8ca487197c4c501928b0fa85f77384f6fea
seemavora <seema.s.vora@gmail.com>
2022-12-05 23:37:26 -0800

    Adds all the Tables

--- hotel-management/src/Pages/Billing/Billing.js
+++ hotel-management/src/Pages/Billing/Billing.js
@@ -1,40 +1,74 @@
-import React from "react";
+import React,{Component} from "react";
 import { Container, Row, Col, Button } from "reactstrap";
 import BillingCard from "../../Components/BillingCard/BillingCard";
 import "./billing.css";
+import { getBilling } from "../../api/test.api";
 
 export default class Billing extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {};
-    }
+		super(props);
+		this.state = {
+			test: []
+		};
+	}
 
-    componentDidMount() {
-        this.initTheme();
-    }
+	componentDidMount() {
+		console.log("componentDidMount entered")
 
-    initTheme() {
-        let theme = JSON.parse(localStorage.getItem("theme"));
-        theme && this.setState({ theme: theme });
-    }
+		getBilling().then(response => {
+			console.log(response)
+			this.setState({ test: response.data })
+			console.log("billing", this.state.test)
+			
+
+		})
+		this.initTheme();
+	}
+
+	initTheme() {
+		let theme = JSON.parse(localStorage.getItem("theme"));
+		theme && this.setState({ theme: theme });
+	}
 
     render() {
+        var heading = ["Credit Card ID", "Card Number","Card Name","Card Type","Expiry Date"];
+		var body = this.state.test
         return (
-            <div className="billing-background">
-                <h1 style={{ padding: "15px" }}>All Billing Information</h1>
-                <Container>
-                    <Row>
-                        <Col>
-                            <BillingCard />
-                        </Col>
-                    </Row>
-                    <Row className="bill-cards-structure">
-                        <Col>
-                            <BillingCard />
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        );
-    }
+			<div className="reservations-background">
+				<div className="card-wrapper">
+					<Table heading={heading} body={body} />
+				</div>
+
+			</div>
+		);
+	}
+}
+class Table extends Component {
+	render() {
+		var heading = this.props.heading;
+		var body = this.props.body;
+		return (
+			<table className ="center-table" style={{ width: 500 }}>
+				<thead >
+					<tr className ='rowLines'>
+						{heading.map(head => <th>{head}</th>)}
+					</tr>
+				</thead>
+				<tbody className ='rowLines'>
+					{body.map(row => <TableRow row={row} />)}
+				</tbody>
+			</table>
+		);
+	}
+}
+
+class TableRow extends Component {
+	render() {
+		var row = this.props.row;
+		return (
+			<tr className ='rowLines'>
+				{row.map(val => <td>{val}</td>)}
+			</tr>
+		)
+	}
 }
--- hotel-management/src/Pages/Employees/Employees.js
+++ hotel-management/src/Pages/Employees/Employees.js
@@ -1,40 +1,74 @@
-import React from "react";
+import React, {Component} from "react";
 import { Container, Row, Col, Button } from "reactstrap";
 import EmployeeCard from "../../Components/EmployeeCard/EmployeeCard";
 import "./employees.css";
+import { getEmployee } from "../../api/test.api";
 
 export default class Employees extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {};
-    }
+		super(props);
+		this.state = {
+			test: []
+		};
+	}
 
-    componentDidMount() {
-        this.initTheme();
-    }
+	componentDidMount() {
+		console.log("componentDidMount entered")
 
-    initTheme() {
-        let theme = JSON.parse(localStorage.getItem("theme"));
-        theme && this.setState({ theme: theme });
-    }
+		getEmployee().then(response => {
+			console.log(response)
+			this.setState({ test: response.data })
+			console.log("yoyo", this.state.test)
+			
+
+		})
+		this.initTheme();
+	}
+
+	initTheme() {
+		let theme = JSON.parse(localStorage.getItem("theme"));
+		theme && this.setState({ theme: theme });
+	}
 
     render() {
+        var heading = ['Employee Id', 'First Name', 'Last Name', 'Address', 'Email', 'Cell Phone Number', 'Role','SSN'];
+		var body = this.state.test
         return (
-            <div className="employees-background">
-                <h1 style={{ padding: "15px" }}>All Employees</h1>
-                <Container>
-                    <Row>
-                        <Col>
-                            <EmployeeCard />
-                        </Col>
-                    </Row>
-                    <Row className="emp-cards-structure">
-                        <Col>
-                            <EmployeeCard />
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        );
-    }
+			<div className="reservations-background">
+				<div className="card-wrapper">
+					<Table heading={heading} body={body} />
+				</div>
+
+			</div>
+		);
+	}
+}
+class Table extends Component {
+	render() {
+		var heading = this.props.heading;
+		var body = this.props.body;
+		return (
+			<table className ="center-table" style={{ width: 900 }}>
+				<thead >
+					<tr className ='rowLines'>
+						{heading.map(head => <th>{head}</th>)}
+					</tr>
+				</thead>
+				<tbody className ='rowLines'>
+					{body.map(row => <TableRow row={row} />)}
+				</tbody>
+			</table>
+		);
+	}
+}
+
+class TableRow extends Component {
+	render() {
+		var row = this.props.row;
+		return (
+			<tr className ='rowLines'>
+				{row.map(val => <td>{val}</td>)}
+			</tr>
+		)
+	}
 }
